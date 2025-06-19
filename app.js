faf970c9a6f2bbc53de728bad02248612fe46c6c
Leonardo Pires <165698194+leonardojpires@users.noreply.github.com>
2025-06-19 15:24:31 +0100

    Added enrollments route

--- app.js
+++ app.js
@@ -124,6 +124,69 @@ app.delete('/courses/:id', (req, res) => {
     });
 });
 
+// -- ENROLLMENTS --
+app.get('/enrollments', (req, res) => {
+    const query = `
+        SELECT e.id, u.name AS user_name, c.title AS course_title, e.enrolled_at
+        FROM enrollments e
+        JOIN users u ON e.user_id = u.id
+        JOIN courses c ON e.course_id = c.id
+    `;
+
+    con.query(query, (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.json(result);
+    });
+});
+
+/* app.get('/courses/:id', (req, res) => {
+    const courseId = req.params.id;
+    const query = 'SELECT * FROM courses WHERE id = ?';
+
+    con.query(query, [courseId], (err, result) => {
+        if (err) return res.status(500).send(err);
+        if (result.length === 0) return res.status(400).send('Course not found!');
+        res.json(result[0]);
+    });
+}); */
+
+app.post('/enrollments', (req, res) => {
+    const { user_id, course_id } = req.body;
+    const query = 'INSERT INTO enrollments (user_id, course_id) VALUES (?, ?)';
+
+    con.query(query, [user_id, course_id], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.status(201).json({ id: result.insertId, user_id, course_id });
+    });
+});
+
+app.put('/enrollments/:id', (req, res) => {
+    const enrollmentId = req.params.id;
+    const { user_id, course_id } = req.body;
+    const query = 'UPDATE enrollments SET user_id = ?, course_id = ? WHERE id = ?';
+
+    con.query(query, [ user_id, course_id, enrollmentId ], (err, result) => {
+        if (err) return res.status(500).send(err);
+        if (result.affectedRows === 0) return res.status(404).send('Enrollment not found!');
+        res.status(200).json({ 
+            message: 'Enrollment updated successfully!',
+            user: {  enrollmentId, user_id, course_id }
+        });
+    });
+});
+
+app.delete('/enrollments/:id', (req, res) => {
+    const enrollmentId = req.params.id;
+    const query = 'DELETE FROM enrollments WHERE id = ?';
+
+    con.query(query, [enrollmentId], (err, result) => {
+        if (err) return res.status(500).send(err);
+        if (result.affectedRows === 0) return res.status(404).send('Enrollment not found!');
+        res.status(200).json({ 
+            message: 'Enrollment deleted successfully!',
+        });
+    });
+});
 
 let server = app.listen(3000, () => {
     console.log(`Server is running on port http://localhost:3000`);
